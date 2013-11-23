ed184f082e7ba212194a3c9360bf8f3bf42907d1
Juline Lamusga <juline.lamusga@gmail.com>
2013-11-23 01:34:39 +0000

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikijuju::Application.config.secret_token = '3550782fb30188b39c51578192ccc8980bc8c1c9ef72f947c6c2399c0a16483d5469dd8c4c3a9c28bfa227ccc9aed3293a7c691d3891464b2fcd14ff16341133'
