4e80345b1e2372e1200bbba6e2b5a956c86d879e
Juline Lamusga <juline.lamusga@gmail.com>
2013-11-26 05:49:23 +0000

    made changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
   def new
     @page = Page.new
+    @wiki = Wiki.find(params[:wiki_id])
   end
 
   def create
-    @page = Page.new(params[:page])
+    pg = params[:page]
+    @page = Page.new
+    @page.title = pg[:title]
+    @page.body = pg[:body]
+    @page.wiki_id = pg[:wiki]
+    @page.user_id = current_user.id
     if @page.save
       flash[:notice] = "Your page was saved."
       redirect_to @page
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :wiki
   belongs_to :wiki
   belongs_to :user
 end
