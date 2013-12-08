c3a06a51c41a04483647faccec5f79306f58d7a7
Juline Lamusga <juline.lamusga@gmail.com>
2013-12-08 21:39:12 +0000

    finished collaborations create

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -3,6 +3,20 @@ class CollaboratorsController < ApplicationController
   end
 
   def create
-    raise
+    @user = User.find_by_email(params[:email])
+    if @user
+      @wiki = Wiki.find(params[:wiki_id])
+      if @user.collaborations.where(wiki_id: @wiki.id).present?
+        flash.now[:error] = "That user is already a collaborator on this JuJu."
+        render :new
+      else
+        @user.collaborations.create(wiki_id: @wiki.id)
+        flash[:notice] = "Collaboration created successfully."
+        redirect_to wiki_path(@wiki)
+      end
+    else
+      flash.now[:error] = "No user with that email address could be found."
+      render :new
+    end
   end
 end
--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,4 +1,5 @@
 class Collaboration < ActiveRecord::Base
+  attr_accessible :wiki_id
   belongs_to :wiki
   belongs_to :user
 end
